aeafaaff13e5bc0165cb7af60b946f84e7ca275f
Sathwick reddy <sathwickreddyy@outlook.com>
2024-12-19 14:31:30 +0530

    TLS Server Setup

---
+++ java-tls-setup/src/main/java/com/java/javatlssetup/TLSServerController.java
@@ -0,0 +1,12 @@
+package com.java.javatlssetup;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TLSServerController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, from TLS Server!";
+    }
+}
