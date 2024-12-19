9ddd41fa6aee9be0a91571169ef6a32719421a41
Sathwick reddy <sathwickreddyy@outlook.com>
2024-12-19 14:04:43 +0530

    Initialise Repository

---
+++ java-tls-setup/src/main/java/com/java/javatlssetup/JavaTlsSetupApplication.java
@@ -0,0 +1,13 @@
+package com.java.javatlssetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTlsSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTlsSetupApplication.class, args);
+    }
+
+}
