93abecd28a1d0ad59945c2057568d140d8ccba6f
Sathwick reddy <sathwickreddyy@outlook.com>
2024-12-20 10:22:00 +0530

    Refactor code for .p12 certificate

--- java-tls-setup/src/main/java/com/java/javatlssetup/TLSServerController.java
+++ java-tls-setup/src/main/java/com/java/javatlssetup/TLSServerController.java
@@ -1,10 +1,12 @@
 package com.java.javatlssetup;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TLSServerController {
+    @CrossOrigin(origins = {"http://localhost:3000", "http://192.168.1.3:3000"}, allowedHeaders = "*", allowCredentials = "true")
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, from TLS Server!";
