7e7786c414617802a7a983acbe242990819b37c9
Sathwick reddy <sathwickreddyy@outlook.com>
2024-12-26 00:00:22 +0530

    Client Setup for TLS testing

---
+++ java-tls-setup-client/src/main/java/com/java/javatlssetupclient/JavaTlsSetupClientApplication.java
@@ -0,0 +1,13 @@
+package com.java.javatlssetupclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTlsSetupClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTlsSetupClientApplication.class, args);
+    }
+
+}
