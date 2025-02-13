3bc1f65a2ae14e49e2c4a0c50eedf315f9971bb5
Sathwick reddy <sathwickreddyy@outlook.com>
2025-02-13 18:46:19 +0530

    Update AuthController

--- ticket-booking-system/user-service/src/main/java/com/java/ticketbookingsystem/userservice/controller/AuthController.java
+++ ticket-booking-system/user-service/src/main/java/com/java/ticketbookingsystem/userservice/controller/AuthController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
